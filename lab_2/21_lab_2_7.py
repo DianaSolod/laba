83bb34f1a20af24963c4da77e94f9de4f72f4c16
DianaSolod <61231171+DianaSolod@users.noreply.github.com>
2020-04-02 17:12:49 +0300

    Add files via upload

---
+++ lab_2/21_lab_2_7.py
@@ -0,0 +1,16 @@
+def leon(n):
+    if n == 0 or n == 1:
+        return 1
+    return leon(n - 1) + leon(n - 2) + 1
+
+#запуск функции и взаимодействие с пользователем
+a = 1
+while(a != 'e'):
+    a = input('Введите номер числа леонардо (e to exit): ')
+    if a.isnumeric():
+        print(a,'число Леонардо: ',leon(int(a)))
+    elif a == 'e':
+        continue
+    else:
+        print("Wrong input")
+
