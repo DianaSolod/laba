8ba44dd24ef16e274f8e82f66344c852b3932cc1
DianaSolod <61231171+DianaSolod@users.noreply.github.com>
2020-04-02 16:41:08 +0300

    Add files via upload

--- lab_2/21_lab_2_1.py
+++ lab_2/21_lab_2_1.py
@@ -1,1 +1,52 @@
+import math
 
+#функция, которая создает массив или читает его из файла
+def arr(x):
+    if x == 'b':
+        print('Enter size of array')
+        N = int(input())
+        L = [0]*N
+        for i in range(N): 
+           print ( "L[", i, "]=", sep = "", end = "" ) 
+           L[i] = float(input())
+        print("L = ", L)
+        return L
+    else:
+        data = []
+        file = open(input("Enter file path: "))
+        for line in file:
+            data.append([float(x) for x in line.split()])
+        return data[0]
+
+def dec(L):
+    N = len(L)
+    len1 = math.ceil(math.sqrt(N))
+    B = [0]*len1
+    for i in range(N):
+        B[i // len1] += L[i]        #определение суммы каждого блока длиной корень из длины массива
+    print("B = ", B)
+    print('Enter indexes')
+    i = int(input())
+    r = int(input())
+    Sum = 0
+    while i <= r:
+        if i % len1 == 0 and i + len1 - 1 <= r:     #к сумме прибавляется значение блока если блок входит в промежуток
+            Sum += B[i // len1]
+            i += len1
+        else:
+            Sum += L[i]      #иначе (в конце) к сумме прибавляются слагаемые покомпонентно
+            i += 1
+    print('Sum = ' + str(Sum))
+
+
+#запуск функции и взаимодействие с пользователем
+a = 1
+while(a != 'c'):
+    a = input('Type a to load a file or b to enter array (c to exit): ')
+    if a == 'a' or a == 'b':
+        B = arr(a)
+        dec(B)
+    elif a == 'c':
+        continue
+    else:
+        print("Wrong input")
